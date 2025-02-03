a6ee8f6983de19cb7b64a7115d89f87983c4fcbc
Guilherme Wallace <guilherme.wallace13@hotmail.com>
2025-02-03 16:46:29 -0300

    Last update for OUTDOOR

--- outdoor.c
+++ outdoor.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
-#include <string.h>
 #include <ctype.h>
 
-#define MAX_LETRAS 20
-#define ALTURA_LETRA 5
-#define LARGURA_LETRA 5
-
-const char LETRAS[26][ALTURA_LETRA][LARGURA_LETRA + 1] = {
+const char LETRAS[26][5][6] = {
     {"  #  ", " # # ", "#####", "#   #", "#   #"}, // A
     {"#### ", "#   #", "#### ", "#   #", "#### "}, // B
     {" ####", "#    ", "#    ", "#    ", " ####"}, // C
@@ -35,19 +30,17 @@ const char LETRAS[26][ALTURA_LETRA][LARGURA_LETRA + 1] = {
     {"#####", "   # ", "  #  ", " #   ", "#####"}  // Z
 };
 
-int caractere_para_indice(char c) {
-    return toupper(c) - 'A';
-}
-
-void exibir_palavra_led(const char *palavra) {
-    int tamanho = strlen(palavra);
-    for (int linha = 0; linha < ALTURA_LETRA; linha++) {
-        for (int i = 0; i < tamanho; i++) {
-            int indice = caractere_para_indice(palavra[i]);
-            if (indice < 0 || indice >= 26) {
-                printf("N/A");
+void exibir_palavra(char *palavra) {
+    for (int linha = 0; linha < 5; linha++) {
+        for (int i = 0; palavra[i] != '\0'; i++) {
+            char c = palavra[i];
+            if (c >= 'a' && c <= 'z') {
+                c -= 32;
+            }
+            if (c >= 'A' && c <= 'Z') {
+                printf("%s  ", LETRAS[c - 'A'][linha]);
             } else {
-                printf("%s ", LETRAS[indice][linha]);
+                printf(" N/A ");
             }
         }
         printf("\n");
@@ -55,22 +48,21 @@ void exibir_palavra_led(const char *palavra) {
 }
 
 int main() {
-    char palavra[MAX_LETRAS + 1];
+    char palavra[21];
     int valido;
     do {
         valido = 1;
-        printf("Digite uma palavra de no máximo %d letras, apenas de A-Z: ", MAX_LETRAS);
-        scanf("%s", palavra);
+        printf("Digite uma palavra de no máximo 20 letras, apenas de A-Z: ");
+        scanf("%20s", palavra);
         for (int i = 0; palavra[i] != '\0'; i++) {
             if (!isalpha(palavra[i])) {
                 valido = 0;
-                printf("Erro: A palavra deve conter apenas letras de A-Z.\n");
+                printf("Erro: A palavra deve conter apenas letras de A-Z, sem acentos e sem Ç.\n");
                 break;
             }
         }
     } while (!valido);
     
-    exibir_palavra_led(palavra);
-    
+    exibir_palavra(palavra);
     return 0;
 }
