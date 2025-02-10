3d7219ce9b47a23e697583efa147fc7c71369e96
Guilherme Wallace <guilherme.wallace13@hotmail.com>
2025-02-10 20:17:24 -0300

    Add list of cousin numbers

--- MMC.c
+++ MMC.c
@@ -1,74 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-// Faz o MMC entre 2 numeros
-int mmc(int priNum, int segNum) {
-    int maiorNum;
-    if (priNum > segNum) {
-        maiorNum = priNum;
-    } else {
-        maiorNum = segNum;
-    }
-
-    int mmc = maiorNum;
-    while ((mmc % priNum != 0) || (mmc % segNum != 0)) {
-        mmc = mmc + maiorNum;
-    }
-    return mmc;
+int mmc(int a, int b) {
+    int m = a > b ? a : b;
+    while (m % a || m % b) m++;
+    return m;
 }
 
-// Faz o MMC do array
-int arrayMMC(int numeros[], int qtdNum) {
-    int resultado = numeros[0];
-    for (int i = 1; i < qtdNum; i++) {
-        resultado = mmc(resultado, numeros[i]);
-    }
-    return resultado;
+int calculaMMC(int *nums, int n) {
+    int res = nums[0];
+    for (int i = 1; i < n; i++)
+        res = mmc(res, nums[i]);
+    return res;
 }
 
-// Pega o número digitado e confere se ele e inteiro
-int pegaInteiro() {
-    int numero;
-    while (1) {
-        if (scanf("%d", &numero) == 1) {
-            return numero;
-        } else {
-            printf("Favor informar um numero interiro! ");
-
-            while (getchar() != '\n');
+void primos(int num) {
+    printf("Os primos são: ");
+    for (int i = 2; num > 1; i++) {
+        while (num % i == 0) {
+            printf("%d ", i);
+            num /= i;
         }
     }
+    printf("\n");
 }
 
 int main() {
-    int qtdNum;
-    printf("MMC de quantos numeros? ");
-    qtdNum = pegaInteiro();
+    int qtd;
+    printf("Quantos números? ");
+    scanf("%d", &qtd);
+    if (qtd < 2) return 1;
 
-    if (qtdNum < 2) {
-        printf("Precisa ser informado no minimo 2 numeros.\n");
-        return 1;
-    }
+    int nums[qtd];
+    printf("Digite os números: ");
+    for (int i = 0; i < qtd; i++)
+        scanf("%d", &nums[i]);
 
-    // Alocação dinâmica do array
-    // substitui o int numeros[qtdNum];
-    int *numeros = (int *)malloc(qtdNum * sizeof(int));
-    if (numeros == NULL) {
-        printf("Erro ao alocar memória.\n");
-        return 1;
-    }
-
-    printf("Digite os %d numeros:\n", qtdNum);
-    printf("Obs: A cada numero inserido aperte Enter. \n\n");
-    for (int i = 0; i < qtdNum; i++) {
-        numeros[i] = pegaInteiro();
-    }
-
-    int resultado = arrayMMC(numeros, qtdNum);
-    printf("O MMC desses numeros e: %d\n", resultado);
-
-    // Liberar a memória alocada
-    free(numeros);
-
+    int resultado = calculaMMC(nums, qtd);
+    printf("O MMC é: %d\n", resultado);
+    primos(resultado);
+    
     return 0;
 }
